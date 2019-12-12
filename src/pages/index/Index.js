8479c4a2e26d51690ccb38d7855f20ae9345d687
jay <skymancn@gmail.com>
2019-12-12 16:57:19 +0800

    Added the image for person list page.

--- src/pages/index/Index.js
+++ src/pages/index/Index.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./index.less";
+import PERSON_IMAGE from "./../../assets/images/idcard-icon.png";
 
 import {
   Form,
@@ -13,7 +14,7 @@ import {
   Checkbox,
   Button,
   AutoComplete,
-  Card
+  Pagination
 } from "antd";
 
 const { Option } = Select;
@@ -28,23 +29,65 @@ class Index extends Component {
     return (
       <div className="ja-layout">
         <div className="ja-content">
-          <div style={{ padding: "15px 30px" }}>
+          <div style={{ padding: "15px 30px" }} className="ja-content-inner">
             <ul className="ja-person-card">
-                <li>
-
-                </li>
-                <li className="selected">
-
-                </li>
-                <li>
-
-                </li>
-                <li className="selected">
-
-                </li>
-                <li className="selected">
-
-                </li>
+              <li>
+                <div className="ja-person-info">
+                  <img src="./logo192.png"></img>
+                  <ul className="ja-person-info-list">
+                    <li className="user-name">曹平</li>
+                    <li>
+                      <i className="ja-icon ja-idcard-icon"></i>
+                      330480198703091203
+                    </li>
+                    <li>
+                      <i className="ja-icon ja-circle-with-wave-icon"></i>
+                      连续18天未出现
+                    </li>
+                  </ul>
+                </div>
+              </li>
+              <li className="selected">
+                <div className="ja-person-info">
+                  <img src="./logo192.png"></img>
+                  <ul className="ja-person-info-list">
+                    <li className="user-name">曹平</li>
+                    <li>
+                      <i className="ja-icon ja-idcard-icon"></i>
+                      330480198703091203
+                    </li>
+                    <li>
+                      <i className="ja-icon ja-circle-with-wave-icon"></i>
+                      连续18天未出现
+                    </li>
+                  </ul>
+                </div>
+              </li>
+              <li></li>
+              <li className="selected"></li>
+              <li className="selected"></li>
+            {/* </ul>
+            <ul className="ja-person-card"> */}
+              <li>
+                <div className="ja-person-info">
+                  <img src="./logo192.png"></img>
+                  <ul className="ja-person-info-list">
+                    <li className="user-name">曹平</li>
+                    <li>
+                      <i className="ja-icon ja-idcard-icon"></i>
+                      330480198703091203
+                    </li>
+                    <li>
+                      <i className="ja-icon ja-circle-with-wave-icon"></i>
+                      连续18天未出现
+                    </li>
+                  </ul>
+                </div>
+              </li>
+              <li className="selected"></li>
+              <li></li>
+              <li className="selected"></li>
+              <li className="selected"></li>
             </ul>
           </div>
         </div>
@@ -90,7 +133,36 @@ class Index extends Component {
             </Col>
           </Row>
         </div>
-        <div className="ja-footer">Footer</div>
+        <div className="ja-footer">
+          <Row>
+            <Col span={6}>
+              <span className="ja-text-middle">共94条数据</span>
+              <span className="ja-text-middle">每页</span>
+              <Select defaultValue="lucy" style={{ width: 60 }}>
+                <Option value="jack">10</Option>
+                <Option value="lucy">20</Option>
+                <Option value="Yiminghe">yiminghe</Option>
+              </Select>
+            </Col>
+            <Col span={18}>
+              <div style={{ float: "right", lineHeight: "30px" }}>
+              <Button className="ja-float-right">跳转</Button>
+              
+              <span className="ja-text-middle ja-float-right">/5页</span>
+              <Input
+                className="ja-float-right"
+                style={{ width: "40px", margin: "0 10px" }}
+              ></Input>
+              <span className="ja-text-middle ja-float-right">跳转至</span>
+              <Pagination
+                defaultCurrent={1}
+                total={50}
+                className="ja-float-right"
+              />
+              </div>
+            </Col>
+          </Row>
+        </div>
       </div>
     );
   }
